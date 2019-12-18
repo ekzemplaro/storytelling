db844e28f1604db0c04a47d50f248d897d7e2820
ekzemplaro <uchida@ekzemplaro.org>
2019-12-18 15:20:51 +0900

    Commit on Dec/18/2019

--- go_upload.sh
+++ go_upload.sh
@@ -1,9 +1,9 @@
 #
-# sudo ufw default allow outgoing
+sudo ufw default allow outgoing
 #
 git add --all
 git commit -m "Commit on Dec/18/2019"
 git push -u origin master
 #
-# sudo ufw default deny outgoing
+sudo ufw default deny outgoing
 #
