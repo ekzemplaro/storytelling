d973c412bdd345ea39f07f21ab42c58704ce4e08
ekzemplaro <linux@hi-ho.ne.jp>
2022-01-01 10:47:45 +0900

    Commit on Jan/01/2022

--- go_upload.sh
+++ go_upload.sh
@@ -2,7 +2,7 @@
 #sudo ufw default allow outgoing
 #
 git add --all
-git commit -m "Commit on Dec/23/2021"
+git commit -m "Commit on Jan/01/2022"
 git push -u origin master
 #
 #sudo ufw default deny outgoing
