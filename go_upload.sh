114e277e70494f43987ad5abdb5f9a651dd5bf33
ekzemplaro <linux@hi-ho.ne.jp>
2022-03-14 19:09:33 +0900

    Commit on Mar/14/2022

--- go_upload.sh
+++ go_upload.sh
@@ -2,7 +2,7 @@
 #sudo ufw default allow outgoing
 #
 git add --all
-git commit -m "Commit on Feb/21/2022"
+git commit -m "Commit on Mar/14/2022"
 git push -u origin master
 #
 #sudo ufw default deny outgoing
